249d59afb2c2d9c6b9249190d8f0b6d7fb86cb5e
DarkByte <panov210@yandex.ru>
2017-02-11 12:13:03 +0300

    Сделал выбор даты и переключение форм, добавил код для работы с http

---
+++ app/src/main/java/com/example/ilya/jexchangerates/RateActivity.java
@@ -0,0 +1,16 @@
+package com.example.ilya.jexchangerates;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Date;
+
+public class RateActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+        //Date date = new Date(getIntent().getLongExtra("date", 0));
+
+    }
+}
